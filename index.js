d1768bddc091986fda61a79dbef710d48a4fbb46
Momen <touristmomen@gmail.com>
2025-07-03 11:12:57 +0600

    favicon

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const favicon = require("serve-favicon");
+const path = require("path");
 const app = express();
 const router = require("./routes/Routes");
 const welcomeRoute = require("./welcome");
 require("dotenv").config();
 const port = process.env.PORT;
+
+// Serve favicon
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use(
   cors({
@@ -14,7 +19,7 @@ app.use(
 );
 app.use(express.json());
 app.use(router);
-app.use(welcomeRoute); 
+app.use(welcomeRoute);
 
 app.listen(port, () => {
   console.log(`Portfolio is running on http://localhost:${port}`);
